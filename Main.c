0312d841f6c7443ef78682b37a9971d1d26d383e
Unknown <eriksters@gmail.com>
2020-11-27 12:37:11 +0100

    Project Start
    Startup, clock config, main, debug config

---
+++ Main.c
@@ -0,0 +1,43 @@
+#include "stm32f10x.h"
+#include <stdio.h>
+
+/* Slightly modified version of Clock Setup from example project */
+void SystemCoreClockConfigure( void );
+void SystemCoreClockConfigure( void ) {
+	
+  RCC->CR |= ((uint32_t)RCC_CR_HSION);                     // Enable HSI
+  while ((RCC->CR & RCC_CR_HSIRDY) == 0);                  // Wait for HSI Ready
+
+  RCC->CFGR = RCC_CFGR_SW_HSI;                             // HSI is system clock
+  while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);  // Wait for HSI used as system clock
+
+  FLASH->ACR  = FLASH_ACR_PRFTBE;                          // Enable Prefetch Buffer
+  FLASH->ACR |= FLASH_ACR_LATENCY;                         // Flash 1 wait state
+
+  RCC->CFGR |= RCC_CFGR_HPRE_DIV1;                         // HCLK = SYSCLK
+  RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;                        // APB1 = HCLK/2
+  RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;                        // APB2 = HCLK
+
+  RCC->CR &= ~RCC_CR_PLLON;                                // Disable PLL
+
+  //  PLL configuration:  = HSI/2 * 16 = 64 MHz
+  RCC->CFGR &= ~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL);
+  RCC->CFGR |=  (RCC_CFGR_PLLSRC_HSI_Div2 | RCC_CFGR_PLLMULL16);
+
+  RCC->CR |= RCC_CR_PLLON;                                 // Enable PLL
+  while((RCC->CR & RCC_CR_PLLRDY) == 0) __NOP();           // Wait till PLL is ready
+
+  RCC->CFGR &= ~RCC_CFGR_SW;                               // Select PLL as system clock source
+  RCC->CFGR |=  RCC_CFGR_SW_PLL;
+  while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);  // Wait till PLL is system clock src
+}
+
+// Main
+
+int main() {
+	SystemCoreClockConfigure();
+	
+	for (;;) {
+		printf("Hello world\n");
+	}
+}
---
+++ RTE/_STM32F103RB/RTE_Components.h
@@ -0,0 +1,27 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'E_OS' 
+ * Target:  'STM32F103RB' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+/* Keil.ARM Compiler::Compiler:I/O:STDERR:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+          #define RTE_Compiler_IO_STDERR_ITM      /* Compiler I/O: STDERR ITM */
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+
+
+#endif /* RTE_COMPONENTS_H */
