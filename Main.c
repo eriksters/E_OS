c909cff00194561330865857a7cf7b91e986bda5
Unknown <eriksters@gmail.com>
2020-11-29 17:34:55 +0100

    Broken Task Switching

--- Main.c
+++ Main.c
@@ -31,20 +31,146 @@ void SystemCoreClockConfigure( void ) {
   RCC->CFGR |=  RCC_CFGR_SW_PLL;
   while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);  // Wait till PLL is system clock src
 }
+//
 
-// Main
+void EXTI0_IRQHandler( void );
+void EXTI0_IRQHandler( void ) {
+	printf("EXTI 0 \n");
+}
+
+void SysTick_Handler( void );
+void SysTick_Handler( void ) {
+	printf("In SysTick_Handler\n");
+}
+
+typedef struct {
+	uint32_t R0;
+	uint32_t R1;
+	uint32_t R2;
+	uint32_t R3;
+	uint32_t R4;
+	uint32_t R5;
+	uint32_t R6;
+	uint32_t R7;
+	uint32_t R8;
+	uint32_t R9;
+	uint32_t R10;
+	uint32_t R11;
+	uint32_t R12;
+	uint32_t SP;
+	uint32_t LR;
+	uint32_t PC;
+	uint32_t xPSR;
+} os_Registers;
+
+#define TASK_STACK_SIZE		0x200
+#define MAX_TASK_COUNT 		10
+
+os_Registers* task_queue[MAX_TASK_COUNT];
+static uint32_t taskCount;
+static uint32_t currentTask = 0;
+
+extern void Sys_Call( os_Registers* );
+extern void SwitchPSP( void );
+void init_task( void ( *func )( void ), uint8_t* stack, os_Registers* regs );
+extern void start_task( os_Registers* regs );
+extern void os_start( void );
+void os_release( void );
+extern void os_switch( os_Registers* current_task, os_Registers* target_task);
+//  void os_addToQueue( os_Registers* regs);
 
 
-
-extern void Sys_Call( void );
- 
-int main() {
-	SystemCoreClockConfigure();
+//	Initialize task's banked registers
+void init_task( void ( *func )( void ), uint8_t* stack, os_Registers* regs) {
 	
-	SysTick_Config(20000);
+	regs->PC = ( uint32_t ) func;
+	
+	regs->LR = ( uint32_t ) func;				//	TODO - Task End
+	
+	regs->xPSR = 0x01000000;						//	Always in Thumb state
+	
+	stack += TASK_STACK_SIZE;
+	regs->SP = ( uint32_t ) stack;
+	
+	task_queue[taskCount] = regs;
+	taskCount++;
+}
+
+void os_release( void ){
+	
+	printf("OS release\n");
+	
+	os_Registers* target_regs;
+	os_Registers* current_regs;
+	if (currentTask == 0) {
+		target_regs = task_queue[1];
+		current_regs = task_queue[0];
+		currentTask = 1;
+	} else {
+		target_regs = task_queue[0];
+		current_regs = task_queue[1];
+		currentTask = 0;
+	}
+	
+	os_switch(current_regs, target_regs);
+}
+
+
+
+os_Registers t1_reg;
+uint8_t t1_stack[TASK_STACK_SIZE];
+void t1_func( void ) __attribute__((noreturn)); 
+
+os_Registers t2_reg;
+uint8_t t2_stack[TASK_STACK_SIZE];
+void t2_func( void ) __attribute__((noreturn));
+
+
+void t1_func( void ) {
+	for (;;) {
+		for (int j = 0; j < 10; j++) {		//	Execute 10 times and then release
+			printf("t1 func. Count = %d\n", j);
+			for (int i = 0; i < 10000; i++) {
+				__NOP();
+			}
+		}
+		os_release();
+	}
+}
+
+void t2_func( void ) {
+	for (;;) {
+		for (int j = 0; j < 20; j++) {		//	Execute 20 times and then release
+			printf("t2 func. Count = %d\n", j);
+			for (int i = 0; i < 10000; i++) {
+				__NOP();
+			}
+		}
+		os_release();
+	}
+}
+
+int main() {
+	//	Clock setup
+	SystemCoreClockConfigure();
+	// SysTick_Config(0x00FFFFFF);
+	
+	//	Switch to PSP
+	//  SwitchPSP();
+	
+	NVIC_EnableIRQ(EXTI0_IRQn);
+	NVIC_SetPendingIRQ(EXTI0_IRQn);
+	
+	init_task(&t1_func, t1_stack, &t1_reg);
+	init_task(&t2_func, t2_stack, &t2_reg);
+	
+	os_start();
+	
+	//  start_task(&t1_reg);
+	
 	
 	//	Trigger System Call
-	Sys_Call();
+	Sys_Call(&t1_reg);
 	
 	for (;;) {
 		printf("Hello world\n");
--- Sys_Call.s
+++ Sys_Call.s
@@ -3,7 +3,143 @@
 				
 				EXPORT 	Sys_Call
 				EXPORT 	SVC_Handler
+				EXPORT	SwitchPSP
+				EXPORT  start_task
+				EXPORT	os_start
+				EXPORT	os_switch
 					
+os_switch		PROC							;	r0 = pointer to current task, r1 = pointer to target task
+	
+				STR		r0, [r0]
+				STR		r1, [r0, #0x4]
+				STR		r2, [r0, #0x8]
+				STR		r3, [r0, #0xC]
+				STR		r4, [r0, #0x10]
+				STR		r5, [r0, #0x14]
+				STR		r6, [r0, #0x18]
+				STR		r7, [r0, #0x1C]
+				STR		r8, [r0, #0x20]
+				STR		r9, [r0, #0x24]
+				STR		r10, [r0, #0x28]
+				STR		r11, [r0, #0x2C]
+				STR		r12, [r0, #0x30]
+				STR		sp, [r0, #0x34]
+				STR		lr, [r0, #0x38]
+		;		STR		pc, [r0, #0x3C]			;	PC?
+		
+				MRS		r2, xPSR
+				STR		r2, [r0, #0x40]
+				
+				MOV		r0, r1
+				
+				B		os_dispatch
+	
+				ENDP
+
+os_start		PROC
+	
+				EXTERN	task_queue			
+				LDR		r0, =task_queue			;	Queue
+				LDR		r0, [r0]				; 	First Task In Queue
+				
+				B		os_dispatch
+				
+				ENDP
+				
+os_dispatch 	PROC							;	r0 = task to load
+	
+				;	Load xPSR
+				LDR		r1, [r0, #0x40]
+				MSR		xPSR, r1
+				
+				;	Load SP
+				LDR		sp, [r0, #0x34]
+				
+				;	Push Program Counter so it can be popped later
+				LDR		r1, [r0, #0x3C]
+				PUSH	{r1}
+				
+				;	Load Register Bank
+				LDR		r1, [r0, #0x4]
+				LDR		r2, [r0, #0x8]
+				LDR		r3, [r0, #0xC]
+				LDR		r4, [r0, #0x10]
+				LDR		r5, [r0, #0x14]
+				LDR		r6, [r0, #0x18]
+				LDR		r7, [r0, #0x1C]
+				LDR		r8, [r0, #0x20]
+				LDR		r9, [r0, #0x24]
+				LDR		r10, [r0, #0x28]
+				LDR		r11, [r0, #0x2C]
+				LDR		r12, [r0, #0x30]
+				LDR		lr, [r0, #0x38]
+				
+				POP		{pc}
+	
+				ENDP
+					
+					
+					
+					
+start_task		PROC	; Start an initialized task
+	
+				LDR		r1, [r0, #0x4]
+				LDR		r2, [r0, #0x8]
+				LDR		r3, [r0, #0xC]
+				LDR		r4, [r0, #0x10]
+				LDR		r5, [r0, #0x14]
+				LDR		r6, [r0, #0x18]
+				LDR		r7, [r0, #0x1C]
+				LDR		r8, [r0, #0x20]
+				LDR		r9, [r0, #0x24]
+				LDR		r10, [r0, #0x28]
+				LDR		r11, [r0, #0x2C]
+				LDR		r12, [r0, #0x30]
+				LDR		sp, [r0, #0x34]
+				LDR		lr, [r0, #0x38]	
+				LDR		pc, [r0, #0x3C]
+	
+				ENDP
+					
+					
+					
+Reg_Store		PROC	; Store register bank in registers struct
+						; R0 must be pointer to struct
+						
+				
+						
+				STR		r1, [r0, #0x4]
+				STR		r2, [r0, #0x8]
+				STR		r3, [r0, #0xC]
+				STR		r4, [r0, #0x10]
+				STR		r5, [r0, #0x14]
+				STR		r6, [r0, #0x18]
+				STR		r7, [r0, #0x1C]
+				STR		r8, [r0, #0x20]
+				STR		r9, [r0, #0x24]
+				STR		r10, [r0, #0x28]
+				STR		r11, [r0, #0x2C]
+				STR		r12, [r0, #0x30]
+				STR		sp, [r0, #0x34]
+				STR		lr, [r0, #0x38]
+				; STR		pc, [r0, #0x3C]
+				MRS		r1, xPSR
+				STR		r1, [r0, #0x40]
+				
+				BX		lr
+				
+				ENDP
+
+SwitchPSP		PROC
+				
+				MOV 	r1, sp
+				MOV		r0, #0x2
+				MSR 	CONTROL, r0
+				MOV		sp, r1
+	
+				ENDP
+				
+
 SVC_Handler		PROC
 	
 				;	Disable all exceptions (except NMI and HardFault)
@@ -26,7 +162,12 @@ SVC_Handler		PROC
 				
 				
 				
+				EXTERN	t1_reg
+				LDR		r0, =t1_reg
+				PUSH	{lr}
+				BL		Reg_Store
 				
+				POP		{lr}
 				BX		lr
 				ENDP
 					
