d57c2ed7abd85ea00a1943702971ad9a9e688126
Unknown <eriksters@gmail.com>
2020-12-06 16:24:21 +0100

    Clean up Main

--- Main.c
+++ Main.c
@@ -45,118 +45,6 @@ void SysTick_Handler( void ) {
 	printf("In SysTick_Handler\n");
 }
 
-/*
-
-#define TASK_STACK_SIZE		0x200
-#define MAX_TASK_COUNT 		10
-
-typedef struct {
-	uint32_t R4;
-	uint32_t R5;
-	uint32_t R6;
-	uint32_t R7;
-	uint32_t R8;
-	uint32_t R9;
-	uint32_t R10;
-	uint32_t R11;
-	uint32_t SP;
-} os_Registers;
-
-typedef struct {
-	uint32_t R0;
-	uint32_t R1;
-	uint32_t R2;
-	uint32_t R3;
-	uint32_t R12;
-	uint32_t LR;
-	uint32_t PC;
-	uint32_t xPSR;
-} os_stackedReg;
-
-typedef struct {
-	uint32_t taskCount;
-	uint32_t currentTask;
-} os_control;
-
-*/
-
-// os_control control;
-// os_Registers* task_queue[MAX_TASK_COUNT];
-
-
-extern void Sys_Call( void );
-
-/*
-extern void SwitchPSP( void );
-void init_task( void ( *func )( void ), uint8_t* stack, os_Registers* regs );
-extern void start_task( os_Registers* regs );
-void os_start( void );
-void os_release( void );
-extern void os_switch( os_Registers* current_task, os_Registers* target_task);
-void os_task_end( void );
-void os_dispatch( os_Registers* reg);
-*/
-
-/*
-void os_task_end( void ) {								//	TODO
-	printf("Task end\n");
-}
-*/
-
-
-extern void SVC_Handler( void );
-
-/*
-//	Initialize task
-void init_task( void ( *func )( void ), uint8_t* stack, os_Registers* regs) {
-	
-	stack += (TASK_STACK_SIZE - sizeof( os_stackedReg ));
-	
-	os_stackedReg* stackedRegisters = ( os_stackedReg* ) stack;
-	
-	stackedRegisters->R0 = 0x11223344;			//	TODO: task parameters
-	stackedRegisters->R1 = 0x0;
-	stackedRegisters->R2 = 0x0;
-	stackedRegisters->R3 = 0x0;
-	stackedRegisters->R12 = 0x0;
-	stackedRegisters->LR = (uint32_t) &os_task_end;
-	stackedRegisters->PC = (uint32_t) func;
-	stackedRegisters->xPSR = 0x01000000;
-	
-	regs->SP = ( uint32_t ) stack;
-	
-	task_queue[control.taskCount] = regs;
-	control.taskCount++;
-}
-*/
-
-/*
-void os_start( void ) { 
-	__ASM("SVC #0x0");
-}
-*/
-
-/*
-void os_release( void ){
-	
-	printf("OS release\n");
-	
-	os_Registers* target_regs;
-	os_Registers* current_regs;
-	if (control.currentTask == 0) {
-		target_regs = task_queue[1];
-		current_regs = task_queue[0];
-		control.currentTask = 1;
-	} else {
-		target_regs = task_queue[0];
-		current_regs = task_queue[1];
-		control.currentTask = 0;
-	}
-	
-	os_switch(current_regs, target_regs);
-}
-*/
-
 
 static os_Registers_t t1_reg;
 static os_TaskStack_t t1_stack;
@@ -192,7 +80,9 @@ void t2_func( void ) {
 }
 
 int main() {
+	
 	//	Clock setup
+	
 	SystemCoreClockConfigure();
 	/* SysTick_Config(0x00FFFFFF); */
 	
@@ -200,15 +90,7 @@ int main() {
 	
 	os_Start();
 	
-	// init_task(&t1_func, t1_stack, &t1_reg);
-	// init_task(&t2_func, t2_stack, &t2_reg);
-	
-	// os_start();
-	
-	//	Trigger System Call
-	// Sys_Call(&t1_reg);
-	
 	for (;;) {
-		printf("Hello world\n");
+		printf("All Threads Exited \n");
 	}
 }
