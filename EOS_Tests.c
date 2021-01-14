1c1b53a40671542e4fbcd0e0095882c32aa88c50
Unknown <eriksters@gmail.com>
2021-01-14 20:11:37 +0100

    Queue tests update

--- EOS_Tests.c
+++ EOS_Tests.c
@@ -32,79 +32,98 @@ uint32_t os_data_structures_test( void ) {
 
 uint32_t os_queue_test( void ) {
 	
-	uint32_t* test_item = (uint32_t*) 0xF0F0F0F0;
-	
-	uint32_t* test_array[DATA_STRUCTURE_SIZE];
+	uint32_t* test_data_array[DATA_STRUCTURE_SIZE];
+	uint32_t* array[DATA_STRUCTURE_SIZE];
 	os_queue_t test_queue;
 	
+	//	Test data
+	for ( uint32_t i = 0; i < DATA_STRUCTURE_SIZE; i++ ) {
+		test_data_array[i] = (( uint32_t*) 4 ) + i;
+	}
+	
 	//	Init
-	os_queue_h queue = os_queue_init( &test_queue, (void**) test_array, DATA_STRUCTURE_SIZE );
+	os_queue_h queue = os_queue_init( &test_queue, (void**) array, DATA_STRUCTURE_SIZE );
 	
 	if ( queue == 0 ) 
 		return 1; 
 	
 	
 	//	Test add 
-	if ( os_queue_add( queue, test_item ) != 0 ) 
+	if ( os_queue_add( queue, test_data_array[0] ) != 0 ) 
+		return 1;
+	if ( os_queue_add( queue, test_data_array[1] ) != 0 ) 
 		return 1;
 	
 	
 	//	Test size
-	if ( os_queue_size( queue ) != 1 ) 
+	if ( os_queue_size( queue ) != 2 ) 
 		return 1;
 	
 	
 	//	Test contains
-	if ( !os_queue_contains( queue, test_item )) 
+	if ( !os_queue_contains( queue, test_data_array[0] )) 
 		return 1;
 	
 	
 	//	Test peek
-	if ( os_queue_peek( queue ) != test_item ) 
+	if ( os_queue_peek( queue ) != test_data_array[0] ) 
+		return 1;
+	
+	
+	//	Test remove element
+	if ( os_queue_remove_element( queue, test_data_array[1] ) != 0 ) 
+		return 1;
+	
+	if ( os_queue_contains( queue, test_data_array[1] )) 
 		return 1;
 	
 	
 	//	Test remove
-	if ( os_queue_remove( queue ) != test_item ) 
+	if ( os_queue_remove( queue ) != test_data_array[0] ) 
 		return 1;
 	
 	
-	//	Test get sequence / remove element
-	os_queue_add( queue, test_item );
-	os_queue_add( queue, test_item + 1 );
-	
-	if ( os_queue_peek( queue ) != test_item )
-		return 1;
-	
-	if ( os_queue_remove_element( queue, test_item ) != 0 ) 
-		return 1;
-	
-	if ( os_queue_contains( queue, test_item ))
-		return 1;
-	
-	if ( !os_queue_contains( queue, test_item + 1 )) 
-		return 1;
-	
-	if ( os_queue_remove_element( queue, test_item + 1 ) != 0 )
-		return 1;
-	
-	
-	//	Test size limit and wrap-around
-	for ( uint32_t i = 1; i < DATA_STRUCTURE_SIZE + 1; i++ ) {
-		if ( os_queue_add( queue, (uint32_t*) i ) != 0 ) 
+	//	Test sequence 
+	for ( uint32_t i = 0; i < DATA_STRUCTURE_SIZE; i++ ) {
+		if ( os_queue_add( queue, test_data_array[i] ) != 0 ) 
 			return 1;
 	}
 	
-	if ( os_queue_add( queue, (uint32_t*) test_item ) == 0 )
-		return 1;
+	for ( uint32_t i = 0; i < DATA_STRUCTURE_SIZE; i++ ) {
+		if ( os_queue_remove( queue ) != test_data_array[i] )
+			return 1;
+	}
 	
 	
-	//	Test is full
-	if ( !os_queue_is_full( queue ) )
-		return 0;
+	//	Test wrap-around
+	for ( uint32_t j = 0; j < 2; j++ ) {
+	
+		for ( uint32_t i = 0; i < DATA_STRUCTURE_SIZE; i++ ) {
+			if ( os_queue_add( queue, test_data_array[i] ) != 0 ) 
+				return 1;
+		}
+		
+		//	Test size limit
+		if ( os_queue_add( queue, (uint32_t*) test_data_array[0] ) == 0 )
+			return 1;
+		
+		//	Test is full
+		if ( os_queue_is_full( queue ) )
+			return 0;
+	
+		
+		for ( uint32_t i = 0; i < DATA_STRUCTURE_SIZE; i++ ) {
+			if ( os_queue_remove( queue ) != test_data_array[i] )
+				return 1;
+		}
+		
+		for ( uint32_t i = 1; i < DATA_STRUCTURE_SIZE + 1; i++ ) {
+			if ( os_queue_add( queue, (uint32_t*) i ) != 0 ) 
+				return 1;
+		}
+	}
 	
 	return 0;
-	
 }
 
 
@@ -112,11 +131,11 @@ uint32_t os_arraylist_test( void ){
 	
 	uint32_t* test_item = (uint32_t*) 0xF0F0F0F0;
 	
-	uint32_t* test_array[DATA_STRUCTURE_SIZE];
-	os_arraylist_t test_arraylist;
+	uint32_t* array[DATA_STRUCTURE_SIZE];
+	os_arraylist_t arraylist;
 	
 	//	Init
-	os_arraylist_h list = os_arraylist_init( &test_arraylist, (void**) test_array, DATA_STRUCTURE_SIZE );
+	os_arraylist_h list = os_arraylist_init( &arraylist, (void**) array, DATA_STRUCTURE_SIZE );
 	
 	if ( list == 0 ) 
 		return 1; 
